cc7fced7764cb4b35976fb371bb0f59354c73020
Simon Atkins <simon@vault.fan>
2024-01-12 16:19:38 +0000

    Populate select dropdown with Kepler planets from server

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(planetsRouter);
 
