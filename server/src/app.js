bab28310c61246d50d9364326f2347b2a68edbdc
Simon Atkins <simon@vault.fan>
2024-01-12 18:28:15 +0000

    Serve our front end build folder from our server instead of create-react-apps

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..", "public", "index.html")));
 
 module.exports = app;
